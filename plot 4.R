908d75102b4020376b05aae1299e9a148b67c415
npaby <74232124+npaby@users.noreply.github.com>
2020-11-18 00:41:37 +0800

    Create plot 4.R

---
+++ plot 4.R
@@ -0,0 +1,19 @@
+
+importData <- read.csv2("./household_power_consumption.txt")
+subsettedData <- subset(importData,importData$Date== c("1/2/2007","2/2/2007"))
+dateAndTime <- strptime(paste(subsettedData$Date, subsettedData$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
+par(mfrow = c(2, 2)) 
+##Plot 1
+plot(dateAndTime, as.numeric(subsettedData$Global_active_power), type="l", xlab="", ylab="Global Active Power")
+##Plot 2
+plot(dateAndTime, subsettedData$Voltage, type="l", xlab="Date and Time", ylab="Voltage")
+##Plot 3
+plot(dateAndTime,subsettedData$Sub_metering_1, type="l",ylab="Energy Submettering",xlab="")
+lines(dateAndTime,subsettedData$Sub_metering_2,col="red")
+lines(dateAndTime,subsettedData$Sub_metering_3,col="blue")
+legend("topright",submeters,lty=1,lwd=2,col=subcolors)
+##Plot 4
+plot(dateAndTime, subsettedData$Global_reactive_power, type="l",xlab = "Date and Time",ylab="Global Reactive Power")
+
+png("plot4.png", width=480, height=480)
+dev.off()
